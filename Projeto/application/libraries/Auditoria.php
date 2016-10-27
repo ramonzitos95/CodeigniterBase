9c05632515f3e10a9e760045751150d59d98d590
toor2016 <ramonss.bque@gmail.com>
2016-10-26 22:48:30 -0200

    Realizado alterações para as corretas funções

--- Projeto/application/libraries/Auditoria.php
+++ Projeto/application/libraries/Auditoria.php
@@ -15,7 +15,36 @@
 class Auditoria
 {
 
-    public function gravandoLog($textoLog)
+  public function gravaLog($conteudo){
+        $fp = null;
+        $arquivo = 'C:\temp\teste2.txt';
+        if(!file_exists($arquivo))
+        {
+            echo "Arquivo não existe no diretório marcado";
+        }
+        $fp = fopen($arquivo, 'a');
+        $counteudo =+ $this->pulaLinha();
+        fwrite($fp, $conteudo);
+        fclose($fp);
+    }
+
+    public function lerArquivoLog(){
+        //Definindo o ponteiro para o arquivo
+        $ponteiro = fopen("C:\\temp\\teste2.txt", "r");
+
+        //Abre o aruqivo .txt
+        while(!feof($ponteiro)) {
+            $linha = fgets($ponteiro, 4096);
+            //Imprime na tela o resultado
+            $this->pulaLinha();
+        }
+        //Fecha o arquivo
+        fclose($ponteiro);
+    }
+
+    public function pulaLinha(){
+        echo "<br>";
+    }  public function gravandoLog($textoLog)
     {
         $this->load->model('Auditoria_model');
         $dadosLogin = array(
